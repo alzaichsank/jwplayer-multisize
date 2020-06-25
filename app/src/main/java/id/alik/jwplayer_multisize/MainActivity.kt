cf5e1c5172ffc91da48ac116f01d42d15f37a943
Alza Ichsan K <alzaichsank@gmail.com>
2020-06-25 14:07:20 +0700

    implmenet logic and view of main

--- app/src/main/java/id/alik/jwplayer_multisize/MainActivity.kt
+++ app/src/main/java/id/alik/jwplayer_multisize/MainActivity.kt
@@ -1,11 +1,122 @@
 package id.alik.jwplayer_multisize
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.longtailvideo.jwplayer.events.FullscreenEvent
+import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents.OnFullscreenListener
+import com.longtailvideo.jwplayer.media.playlists.PlaylistItem
+import id.alik.jwplayer_multisize.costume.CallbackScreen
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnFullscreenListener {
+    private var mCallbackScreen: CallbackScreen? = null
+    private var caseVideo = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
     }
+
+
+    private fun initView() {
+        jwplayer.addOnFullscreenListener(this)
+        // Event Logging
+        mCallbackScreen = findViewById(R.id.callback_screen)
+        mCallbackScreen!!.registerListeners(jwplayer)
+        initAction()
+        onPlayerPlay()
+
+    }
+
+    private fun initAction() {
+        rg_size.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.rb_vcm -> {
+                    caseVideo = 0
+                    Log.d("checked casevideo 0", "onClick: $caseVideo")
+                    onPlayerPlay()
+                }
+                R.id.rb_vyb -> {
+                    caseVideo = 1
+                    Log.d("checked casevideo 1", "onClick: $caseVideo")
+                    onPlayerPlay()
+                }
+                R.id.rb_h_for_and_three -> {
+                    caseVideo = 2
+                    Log.d("checked casevideo 2", "onClick: $caseVideo")
+                    onPlayerPlay()
+                }
+                R.id.rb_h_twenty_one_and_nine -> {
+                    caseVideo = 3
+                    Log.d("checked casevideo 3", "onClick: $caseVideo")
+                    onPlayerPlay()
+                }
+                R.id.rb_h_sixteen_and_nine -> {
+                    caseVideo = 4
+                    Log.d("checked casevideo 4", "onClick: $caseVideo")
+                    onPlayerPlay()
+                }
+            }
+        }
+
+    }
+
+    private fun onPlayerPlay() {
+        when (caseVideo) {
+            0 -> {
+                val playlistItem = PlaylistItem.Builder()
+                    .file("https://content.jwplatform.com/videos/DEo3mNCR-KRDSK1u4.mp4")
+                    .title("Vertical Video Mobile Camera ")
+                    .description("Vertical Video Mobile Camera")
+                    .build()
+                jwplayer.load(playlistItem)
+                jwplayer.play()
+            }
+            1 -> {
+                val playlistItem = PlaylistItem.Builder()
+                    .file("https://content.jwplatform.com/videos/DtyUK4cS-KRDSK1u4.mp4")
+                    .title("Vertical Youtube Video Clip")
+                    .description("Vertical Youtube Video Clip")
+                    .build()
+                jwplayer.load(playlistItem)
+                jwplayer.play()
+            }
+            2 -> {
+                val playlistItem = PlaylistItem.Builder()
+                    .file("https://content.jwplatform.com/videos/FhQBYmLN-Yd9Pisb4.mp4")
+                    .title("Horizontal ratio 4:3")
+                    .description("Horizontal ratio 4:3")
+                    .build()
+                jwplayer.load(playlistItem)
+                jwplayer.play()
+            }
+            3 -> {
+                val playlistItem = PlaylistItem.Builder()
+                    .file("https://content.jwplatform.com/videos/aeIsjyFA-Yd9Pisb4.mp4")
+                    .title("Horizontal ratio 21:9")
+                    .description("Horizontal ratio 21:9")
+                    .build()
+                jwplayer.load(playlistItem)
+                jwplayer.play()
+            }
+            4 -> {
+                val playlistItem = PlaylistItem.Builder()
+                    .file("https://content.jwplatform.com/videos/M21Ck8Bi-KRDSK1u4.mp4")
+                    .title("Horizontal ratio 16:9")
+                    .description("Horizontal ratio 16:9")
+                    .build()
+                jwplayer.load(playlistItem)
+                jwplayer.play()
+            }
+        }
+        Log.d("checked height", "checked height: ${jwplayer?.height}")
+        Log.d("checked width", "checked width: ${jwplayer?.width}")
+    }
+
+
+    override fun onFullscreen(fullscreenEvent: FullscreenEvent?) {
+        Log.d("checked", "onFullscreen: ")
+    }
+
 }
